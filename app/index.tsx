f024cdce580bbbd3ede9c6827d72ed58b0f79855
Nikesha Silva <nikesha.silva@innovative-e.com>
2025-07-23 22:40:48 +0530

    Added project stylling setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl font-bold text-accent">Welcome!</Text>
+      <Link href={"/onboarding"}>OnBoarding</Link>
     </View>
   );
 }
